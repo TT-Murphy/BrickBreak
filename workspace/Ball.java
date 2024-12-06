9af1d5ab4e93e56b17cb502ac0dd8586b2245565
TT-Murphy <taiyo.e.murphy@gmail.com>
2024-12-06 13:10:28 +0000

    commit -m "dffd"

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -19,57 +19,84 @@ public class Ball {
 
 	//methods:
 	//getters
+
+	//Pre-condition - xPos must be an integer
+	//post-condition - returns xPos
 	public int getXpos(){
 		return xPos;
 	}
 
+	//Pre-condition - yPos must be an integer
+	//post-condition - returns yPos
 	public int getYpos(){
 		return yPos;
 	}
 
+	//Pre-condition - size must be an integer
+	//post-condition - returns size
 	public int getSize(){
 		return size;
 	}
 
+	//Pre-condition - xVelocitys must be an integer
+	//post-condition - returns xVelocity
 	public int getXvelocity(){
 		return xVelocity;
 	}
 
+	//Pre-condition - yVelocity must be an integer
+	//post-condition - returns yVelocity
 	public int getYvelocity(){
 		return yVelocity;
 	}
 
 	//setters
+
+	//Pre-condition - v must be an integer
+	//post-condition - sets xVelocity to v
 	public void setXVelocity(int v){
 		this.xVelocity = v;
 	}
 
+	//Pre-condition - v must be an integer
+	//post-condition - sets yVelocity to v
 	public void setYVelocity(int v){
 		this.yVelocity = v;
 	}
-
+	
+	//Pre-condition - x must be a positive
+	//post-condition - sets xPos to x
 	public void setX (int x){
 		this.xPos = x;
 	}
 
+	//Pre-condition - y must be a positive
+	//post-condition - sets yPos to y
 	public void setY (int y){
 		this.yPos = y;
 	}
 
-
+	//Pre-condition -
+	//post-condition - makes yVelocity negative of its value
 	public void reverseY (){
 		this.yVelocity = -1*this.yVelocity;
 	}
 
+	//Pre-condition -
+	//post-condition - makes xVelocity negative of its value
 	public void reverseX (){
 		this.xVelocity = -1*this.xVelocity;
 	}
 
+	//Pre-condition -
+	//post-condition - increments xPos and yPos by xVelocity and yVelocity
 	public void move (){
 		this.xPos+=this.xVelocity;
 		this.yPos+=this.yVelocity;
 	}
 
+	//Pre-condition - g must be a graphic
+	//post-condition - makes g red and draws a circle with the dimentions of xPos, yPos, and size. 
 	public void draw (Graphics g){
 		g.setColor(Color.red);
         g.fillOval(xPos, yPos, size, size);;
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -18,35 +18,53 @@ public class Paddle {
 
 	//methods:
 	//getters
+
+	//Pre-condition - xPos must be an integer
+	//post-condition - returns xPos
 	public int getX(){
 		return this.xPos;
 	}
 	
+	//Pre-condition - yPos must be an integer
+	//post-condition - returns yPos
 	public int getY(){
 		return this.yPos;
 	}
 
+	//Pre-condition - width must be an integer
+	//post-condition - returns width
 	public int getWidth(){
 		return this.width;
 	}
 
+	//Pre-condition - height must be an integer
+	//post-condition - returns height
 	public int getHeight(){
 		return this.height;
 	}
 
+	//Pre-condition - velocity must be an integer
+	//post-condition - returns velocity
 	public int getVelocity(){
 		return this.velocity;
 	}
 
 	//setters
+
+	//Pre-condition - x must be a positive
+	//post-condition - sets xPos to x
 	public void setX(int x){
 		this.xPos=x;
 	}
 
+	//Pre-condition - 
+	//post-condition - sets velocity to v
 	public void setVelocity(int v){
 		this.velocity=v;
 	}
 
+	//Pre-condition - n must be an int
+	//post-condition - sets velocity to n/2 or to 2 if velocity >=2, or -2 if velocity <=2
 	public void addVelocity(int n){
 		this.velocity+=n/2;
 		if (this.velocity>=2){
@@ -57,10 +75,14 @@ public class Paddle {
 		}
 	}
 
+	//Pre-condition -
+	//post-condition - increments xPos by velocity
 	public void move (){
 		this.xPos+=this.velocity;
 	}
 
+	//Pre-condition - g must be a graphic
+	//post-condition - makes g red and draws a rect with the dimentions of xPos, yPos, width, and height.
 	public void draw(Graphics g){
 		g.setColor(Color.red);
         g.fillRect(xPos, yPos, width, height);
