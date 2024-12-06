0ed9a3128a173e931de58b023b49862f3d3802ec
TT-Murphy <taiyo.e.murphy@gmail.com>
2024-12-06 12:54:50 +0000

    commit -m submission 12/6

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -2,12 +2,76 @@ import java.awt.*;
 
 public class Ball {
 	//your code here!
-	
-	//don't forget you need instance variables:
-	
-	
-	//constructor(s):
-	
-	//methods:
 
+	//don't forget you need instance variables:
+	private int xVelocity = 1;
+	private int yVelocity = -1;
+	private int xPos;
+	private int yPos;
+	private int size;
+
+	//constructor(s):
+	public Ball (int x, int y, int s){
+		this.size=s;
+		this.xPos=x;
+		this.yPos=y;
+	}
+
+	//methods:
+	//getters
+	public int getXpos(){
+		return xPos;
+	}
+
+	public int getYpos(){
+		return yPos;
+	}
+
+	public int getSize(){
+		return size;
+	}
+
+	public int getXvelocity(){
+		return xVelocity;
+	}
+
+	public int getYvelocity(){
+		return yVelocity;
+	}
+
+	//setters
+	public void setXVelocity(int v){
+		this.xVelocity = v;
+	}
+
+	public void setYVelocity(int v){
+		this.yVelocity = v;
+	}
+
+	public void setX (int x){
+		this.xPos = x;
+	}
+
+	public void setY (int y){
+		this.yPos = y;
+	}
+
+
+	public void reverseY (){
+		this.yVelocity = -1*this.yVelocity;
+	}
+
+	public void reverseX (){
+		this.xVelocity = -1*this.xVelocity;
+	}
+
+	public void move (){
+		this.xPos+=this.xVelocity;
+		this.yPos+=this.yVelocity;
+	}
+
+	public void draw (Graphics g){
+		g.setColor(Color.red);
+        g.fillOval(xPos, yPos, size, size);;
+	}
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,16 +1,68 @@
-import java.awt.Graphics;
 import java.awt.*;
 
 public class Paddle {
 
-	//your code here!
-	
-	//don't forget you need instance variables:
-	
-	
+	private int velocity;
+	private int height;
+	private int xPos;
+	private int yPos;
+	private int width;
+
 	//constructor(s):
-	
+	public Paddle (int x, int y, int w, int h){
+		this.width=w;
+		this.height=h;
+		this.xPos=x;
+		this.yPos=y;
+	}
+
 	//methods:
+	//getters
+	public int getX(){
+		return this.xPos;
+	}
 	
-	
+	public int getY(){
+		return this.yPos;
+	}
+
+	public int getWidth(){
+		return this.width;
+	}
+
+	public int getHeight(){
+		return this.height;
+	}
+
+	public int getVelocity(){
+		return this.velocity;
+	}
+
+	//setters
+	public void setX(int x){
+		this.xPos=x;
+	}
+
+	public void setVelocity(int v){
+		this.velocity=v;
+	}
+
+	public void addVelocity(int n){
+		this.velocity+=n/2;
+		if (this.velocity>=2){
+			this.velocity=2;
+		}
+		if (this.velocity<=-2){
+			this.velocity=-2;
+		}
+	}
+
+	public void move (){
+		this.xPos+=this.velocity;
+	}
+
+	public void draw(Graphics g){
+		g.setColor(Color.red);
+        g.fillRect(xPos, yPos, width, height);
+	}
 }
